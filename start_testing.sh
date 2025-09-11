78d064d235df86c7c6966bb0141dfded69f8635d
Jesse Simpson <jesse.simpson36@gmail.com>
2025-09-10 22:59:15 -0400

    copy labs example
    Signed-off-by: Jesse Simpson <jesse.simpson36@gmail.com>

--- start_testing.sh
+++ start_testing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp kernelci-ci-gkernelci.json.example GBuildbot-worker/.kernelci-ci-gkernelci.json
 cp GBuildbot-master/buildbot-config.yaml.example GBuildbot-master/buildbot-config.yaml
+cp ./GBuildbot-worker/labs.yaml.example ./GBuildbot-worker/labs.yaml
 sed -i "s/your local docker GID/$(cat /etc/group | grep docker | cut -d: -f3)/" docker-compose.yml.template
 python sparser.py || exit $?
 docker compose up -d
